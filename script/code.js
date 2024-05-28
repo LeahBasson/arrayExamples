6e2b360c95bdc62e68825922c2774b8e7c8aa70e
LeahBasson <leahbasson54@gmail.com>
2024-05-28 12:06:26 +0200

    array examples

---
+++ script/code.js
@@ -0,0 +1,16 @@
+//How to create an array 
+
+//Array Literal
+let numbers = [9, 3, 8, 11];
+
+//Array constructor
+let data = new Array ('Khanya', 'John', 15, 18);
+
+//Array.of()
+let myArr1 = Array.of(9,2,4);
+
+//Array.from
+let myArr2 = Array.from(numbers);
+
+//Array.fill()
+let emoji = new Array(4).fill('😁');
